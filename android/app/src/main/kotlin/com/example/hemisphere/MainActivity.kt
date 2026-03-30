38e9cafd455cdd275d6a4a28e914d9f3f5bfd226
Smeet Patil <smeetpatil878@gmail.com>
2026-03-31 03:42:21 +0530

    Post Merge

--- android/app/src/main/kotlin/com/example/hemisphere/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hemisphere/MainActivity.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.example.hemisphere
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-=======
-package com.example.hemisphere
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 345e37f98aab254ec09547299a58d8adbac3233b
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 allprojects {
     repositories {
         google()
@@ -24,30 +23,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-=======
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
->>>>>>> 345e37f98aab254ec09547299a58d8adbac3233b
